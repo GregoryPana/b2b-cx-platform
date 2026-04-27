c0dae46e9db3f171a121ca0e694cb1d9ed7381e5
GregoryPana <gregorypanagary@gmail.com>
2026-04-27 12:08:17 +0400

    Fix nginx deploy script variable escaping

--- scripts/linux/deploy_nginx.sh
+++ scripts/linux/deploy_nginx.sh
@@ -217,16 +217,16 @@ server {
     location /pgadmin/ {
         proxy_pass http://${PGADMIN_HOST}:${PGADMIN_PORT};
 
-        proxy_set_header Host $host;
+        proxy_set_header Host \$host;
         proxy_set_header X-Script-Name /pgadmin;
-        proxy_set_header X-Scheme $scheme;
+        proxy_set_header X-Scheme \$scheme;
 
-        proxy_set_header X-Forwarded-Proto $scheme;
-        proxy_set_header X-Forwarded-Host $host;
-        proxy_set_header X-Forwarded-Port $server_port;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+        proxy_set_header X-Forwarded-Host \$host;
+        proxy_set_header X-Forwarded-Port \$server_port;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
 
-        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Upgrade \$http_upgrade;
         proxy_set_header Connection "upgrade";
 
         proxy_redirect off;
